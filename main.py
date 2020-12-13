28979efc1d36541c2766b9def15b9fe05195c0f0
jaymody <jaykmody@gmail.com>
2020-12-13 00:33:13 -0500

    use batch prediction for evaluation

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import argparse
 
 import numpy as np
-import tensorflow as tf
 from tqdm import tqdm
 
 from helpers import make_data, score_iou
+from train import ClassificationModel, BBRModel, CombinedModel, preprocess_image
 
 
-def evaluate(model_file):
-    model = tf.keras.models.load_model(model_file)
+def evaluate(model, batch_size, num_samples):
+    images, labels = zip(
+        *[make_data() for _ in tqdm(range(num_samples), desc="creating data")]
+    )
+    images = [preprocess_image(img) for img in tqdm(images, desc="preprocessing")]
+    images = np.stack(images)
 
-    ious = []
-    for _ in tqdm(range(1000)):
-        img, label = make_data()
-        pred = model.predict(img[None])  # batch size 1
-        pred = np.squeeze(pred)
-        ious.append(score_iou(label, pred))
+    predictions, _, _ = model.predict_batch(images, batch_size)
 
+    ious = [score_iou(label, pred) for pred, label in zip(predictions, labels)]
     ious = np.asarray(ious, dtype="float")
     ious = ious[~np.isnan(ious)]  # remove true negatives
+    score = (ious > 0.7).mean()
 
     print()
-    print("score:", (ious > 0.7).mean())
+    print(f"score: {score:.5f}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Reproduce model score.")
-    parser.add_argument("--model_file", type=str, default="model.hdf5")
+    parser.add_argument(
+        "--clf_model_file", type=str, default="models/clf/best_model.hdf5"
+    )
+    parser.add_argument(
+        "--reg_model_file", type=str, default="models/clf/best_model.hdf5"
+    )
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--num_samples", type=int, default=1000)
     args = parser.parse_args()
 
-    evaluate(**args.__dict__)
+    clf_model = ClassificationModel.load_model(args.clf_model_file)
+    reg_model = BBRModel.load_model(args.reg_model_file)
+    model = CombinedModel(clf_model, reg_model)
+
+    evaluate(model, args.batch_size, args.num_samples)
